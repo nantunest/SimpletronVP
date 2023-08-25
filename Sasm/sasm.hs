028eba2ea6bfda706bcb8f5fdc4986d59d979eb7
Nelson Terra <na.terra@gmail.com>
2023-08-25 08:02:09 +0100

    sasm.hs printing instructions in hex.

--- Sasm/sasm.hs
+++ Sasm/sasm.hs
@@ -1,26 +1,38 @@
-{-# LANGUAGE ViewPatterns #-}
 import Text.Printf
-import Data.Maybe (fromMaybe)
 
 data Instruction = READ | WRITE | LOAD | STORE | ADD | SUB | DIV | MUL | JMP | BLZ | BEZ | HALT
                   deriving (Enum, Show)
 
-label l
-    | l == "None" = Nothing
-    | otherwise = Just l
+data Line = Line Int Instruction Int String
+            deriving (Show)
 
---line n l i o = putStrLn $ show n ++ show l ++ show i ++ show o
-line :: (PrintfType t4) => Int -> Maybe String -> Instruction -> Int -> t4
-line n (fromMaybe "" -> l) i = printf "%d %s %s 0x%x\n" n  l (show i)
+--label l
+--    | l == "None" = Nothing
+--    | otherwise = Just l
+--
+--line' :: Int -> Instruction -> Int -> String -> String
+--line' n i o l = show n ++ " " ++ show i ++ " " ++ show o ++ " " ++ l ++ "\n"
+--
+--line :: Int -> Instruction -> Int -> String -> (Int, Instruction, Int, String)
+--line n i o l = (n, i, o, l)
+
+instToOpCode :: Instruction -> Int
+instToOpCode i = fromEnum i + 1
+
+assembleLine :: Line -> String
+assembleLine (Line n i o l) = "0x" ++ show (instToOpCode i) ++ printf "%x" o
+
+assemble :: [Line] -> [String]
+--assemble (l:ls) = show l : assemble ls
+--assemble [] = []
+assemble = map assembleLine
 
 var_n1 = 0x0499
 var_n2 = 0x0498
 
-program :: PrintfType a => [a]
+program :: [Line]
 program = [
-    line 00 (label "init") READ var_n1,
-    line 01 (label "None") READ var_n1
+    Line 00 READ var_n1 "init",
+    Line 01 READ var_n2 "",
+    Line 03 LOAD var_n1 ""
     ]
-
-prog :: PrintfType a => a
-prog = line 0 (label "init") READ var_n1
