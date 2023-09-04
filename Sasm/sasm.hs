8a2ddea155f3adbb0a7aff61eeeba64f1ce4f4bc
Nelson Terra <na.terra@gmail.com>
2023-09-04 09:03:53 +0100

    prog to hexdump still some work to do

--- Sasm/sasm.hs
+++ Sasm/sasm.hs
@@ -1,10 +1,19 @@
 import Text.Printf
+import Data.Bits ( Bits(shiftR, shiftL, (.|.), (.&.)) )
+import Data.Binary ( Word16, encode )
+import qualified Data.ByteString.Lazy as B
+import GHC.Builtin.PrimOps (PrimOp(Word16AddOp))
 
 data Instruction = READ | WRITE | LOAD | STORE | ADD | SUB | DIV | MUL | JMP | BLZ | BEZ | HALT
-                  deriving (Enum, Show)
+                  deriving (Enum, Show, Eq)
 
-data Line = Line Int Instruction Int String
+data Label = Label String Int
             deriving (Show)
+
+data Line = Line String Instruction Int
+            deriving (Show)
+
+data ASMKEYS = InProg
 
 --label l
 --    | l == "None" = Nothing
@@ -19,20 +28,51 @@ data Line = Line Int Instruction Int String
 instToOpCode :: Instruction -> Int
 instToOpCode i = fromEnum i + 1
 
-assembleLine :: Line -> String
-assembleLine (Line n i o l) = "0x" ++ show (instToOpCode i) ++ printf "%x" o
+labelIn (Line l _ _) = l
+addressIn (Label _ a) = a
 
-assemble :: [Line] -> [String]
---assemble (l:ls) = show l : assemble ls
---assemble [] = []
+-- Finds the label address given a label, a program and a starting address
+-- Returns the label address
+labelAddress label (line:progs) idx
+    | labelIn line == label = idx
+    | otherwise = labelAddress label progs (idx + 1)
+labelAddress _ [] _ = -1
+
+toLabel l InProg p = labelAddress l p 0
+
+-- Finds the address of all the labels in a program.
+-- Returns [Label]
+labelAddresses (line:ps) pa
+    | labelIn line /= "" = Label (labelIn line) (labelAddress (labelIn line) pa 0) : labelAddresses ps pa
+    | otherwise = labelAddresses ps pa
+labelAddresses [] _ = []
+
+getLabelAddresses prog = labelAddresses prog prog
+
+assembleLine :: Line -> Int
+assembleLine (Line l i o) =  shiftL (instToOpCode i) 12 .|. o
+--assembleLine (Line l i o) = "0x" ++ printf "%x" (instToOpCode i) ++ printf "%.3x" o
+
+assemble :: [Line] -> [Int]
 assemble = map assembleLine
+
+writeIntsToFile :: FilePath -> [Int] -> IO ()
+writeIntsToFile fp is  = B.writeFile fp $ encode (map toWord16 is) where
+                            toWord16::Int -> Word16
+                            toWord16 x = fromIntegral (((x .&. 0xFF00) `shiftR` 8) .|. ((x .&. 0xFF) `shiftL` 8)) 
 
 var_n1 = 0x0499
 var_n2 = 0x0498
 
 program :: [Line]
 program = [
-    Line 00 READ var_n1 "init",
-    Line 01 READ var_n2 "",
-    Line 03 LOAD var_n1 ""
+    Line "Init"     READ var_n1,
+    Line ""         READ var_n2,
+    Line ""         LOAD var_n1,
+    Line ""         SUB var_n2,
+    Line ""         BLZ (toLabel "n2w" InProg program),
+    Line ""         WRITE var_n1,
+    Line ""         JMP (toLabel "end" InProg program),
+    Line "n2w"      WRITE var_n2,
+    Line "end"      HALT 0
     ]
